e3796fe369a29b20300d0afa54172b70a530b592
Jakubs-lab <vejbornyjakub@gmail.com>
2024-03-20 16:56:06 +0100

    přidání odesílání objednávky na email

--- src/components/Configurator.jsx
+++ src/components/Configurator.jsx
@@ -1,4 +1,5 @@
 import { useCustomization } from "../context/Customization"
+import React from "react";
 
 
 
@@ -7,6 +8,30 @@ import { useCustomization } from "../context/Customization"
     const Configurator = () =>{
     const {material,setMaterial,tableWidth,setTableWidth,tableDepth,setTableDepth,tableHeight,setTableHeight,} = useCustomization()
     
+        const [result, setResult] = React.useState("");
+      
+        const onSubmit = async (event) => {
+          event.preventDefault();
+          setResult("Sending....");
+          const formData = new FormData(event.target);
+      
+          formData.append("access_key", "858c1ee4-e959-4d07-bea9-658d85a70312");
+      
+          const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+          });
+      
+          const data = await response.json();
+      
+          if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+          } else {
+            console.log("Error", data);
+            setResult(data.message);
+          }
+        };
       
     
    return(
@@ -15,7 +40,7 @@ import { useCustomization } from "../context/Customization"
         <div className="configurator_section">
         <h2 className="configurator_section_title">ROZMĚRY STOLU</h2>
             <div className="slidecontainer">
-                <output>Šířka stolu: {tableWidth} cm</output>
+                <output name="message" required>Šířka stolu: {tableWidth} cm</output>
                 <input  type="range" min="70" max="200" value={tableWidth} onChange={(e) => setTableWidth(e.target.value)} className="width"/>
             </div>
             <div className="slidecontainer">
@@ -38,6 +63,35 @@ import { useCustomization } from "../context/Customization"
                 </div>
                 
                 </div>
+                <form className="configurator_form" onSubmit={onSubmit}>
+                    
+                <input type="hidden" name="access_key" value="858c1ee4-e959-4d07-bea9-658d85a70312"></input>
+                    
+
+                    
+                    <input type="hidden" name="subject" value="Nová objednávka"/>
+                    <label  className="configurator_input_label" >Jméno 
+                        <input className="configurator_input" type="text" name="name"  />
+                    </label>
+
+                    <label className="configurator_input_label" >Příjmení
+                        <input className="configurator_input" type="text"  name="surename" required />
+                    </label>
+
+                    <label className="configurator_input_label" >Email
+                        <input className="configurator_input email" type="email" name="email" required />
+                    </label>
+
+                    <label className="configurator_input_label" >Telefoní číslo
+                        <input className="configurator_input" type="phone" name="phone_number" />
+                    </label>
+                    <textarea name="message" required value={`Šířka stolu : ${tableWidth}cm Hloubka stolu : ${tableDepth}cm Výška stolu : ${tableHeight}cm Druh dřeva : ${material}`}>
+
+                    </textarea>
+                    <button type="submit" className="configurator_button_submit">Poslat požadavek</button>
+                </form>
+                <span>{result}</span>
+
 
         </div>
     </div>
